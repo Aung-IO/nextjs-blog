59a7ab27367f4008e578600029082de3b1fef9c5
Aung Pyae Kyaw <aungpyaekyawwiz242@gmail.com>
2023-10-30 01:00:01 +0630

    dynamic routes testing

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
-import {getSortedPostsData} from '../lib/posts'
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -12,18 +12,19 @@ export async function getStaticProps() {
   };
 }
 
-export default function Home({allPostsData}) {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Aung Pyae. I'm junior FrontEnd Developer in Creative Coder Myanmar
-          You can contact me on <a href=''>Twitter.</a>
+        <p>
+          Hello, I'm Aung Pyae. I'm junior FrontEnd Developer in Creative Coder
+          Myanmar You can contact me on <a href="">Twitter.</a>
         </p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
